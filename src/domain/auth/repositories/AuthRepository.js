b8fb339f69a34b3b48ec5fa49912436bc78cc2b6
arieftb <arieftb22@gmail.com>
2025-06-21 14:58:26 +0700

    refactor: [arieftb] Get Property of Registration

--- src/domain/auth/repositories/AuthRepository.js
+++ src/domain/auth/repositories/AuthRepository.js
@@ -3,9 +3,9 @@ import { register } from '../../../data/auth/infrastructure/AuthRemoteService.js
 export class AuthRepository {
   async register (registration) {
     return register({
-      name: registration.name.value,
-      email: registration.email.value,
-      password: registration.password.value,
+      name: registration.getName(),
+      email: registration.getEmail(),
+      password: registration.getPassword(),
     });
   }
 }
